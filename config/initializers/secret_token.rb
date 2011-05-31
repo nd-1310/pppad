359365a9af114fefc1a2af17a512394cae5c0e45
Andrei Petrenko <me@apetrenko.com>
2011-05-31 13:47:18 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pppad::Application.config.secret_token = '8183d96c3865b91db4e2879f6205d85aed89e3bd65b335f88eb360fc64b7d4c631b67c5bfd787e8763a5b40f09baafc91188346adb5080b1ea0b7266f9e0231f'
